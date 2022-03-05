e8c31ed522d418a35025a886fb93036a1a2481de
Jon Leahy <jonathanl@newtimesystems.com>
2022-03-05 12:17:06 +0000

    10. Secret Retriever Service

---
+++ src/services/OneTimeSecretRetriever.ts
@@ -0,0 +1,18 @@
+import {SecretRetriever} from "./SecretRetriever";
+import {SecretRepository} from "./SecretRepository";
+import {Secret} from "../domain/models/Secret";
+import {UrlId} from "../domain/models/UrlId";
+import {SecretNotFoundError} from "../domain/errors/SecretNotFoundError";
+
+export class OneTimeSecretRetriever implements SecretRetriever {
+    constructor(private secretRepository: SecretRepository) { }
+
+    async retrieveSecretByUrlId(urlId: UrlId): Promise<Secret> {
+        const secret = await this.secretRepository.getSecretByUrlId(urlId)
+        if (secret === null) throw new SecretNotFoundError()
+        // try to retrieve a secret
+        // if found return secret and remove the secret
+        // if not found throw an error
+        return null
+    }
+}
