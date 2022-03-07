c559a9935ffd118e4d7986e988fd0670bcd4bbbc
Jon Leahy <jonathanl@newtimesystems.com>
2022-03-07 12:48:13 +0000

    minor formatting

--- test/integration/GetSecrets.int.test.ts
+++ test/integration/GetSecrets.int.test.ts
@@ -8,13 +8,19 @@ describe('Get secrets integration tests', () => {
     it('should return an error when the urlId is too short', async () => {
         const response = await request.get("/api/v1/secrets/short")
         expect(response.status).toBe(400);
-        expect(response.body).toEqual({name: "UrlIdValidationError", message: "UrlId is too short"});
+        expect(response.body).toEqual({
+                name: "UrlIdValidationError",
+                message: "UrlId is too short"
+            });
     });
     it('should return an error when the secret doesn\'t exist', async () => {
         SecretModel.findOne = jest.fn().mockResolvedValue(null)
         const response = await request.get("/api/v1/secrets/nottheresodoesntexist")
         expect(response.status).toBe(404);
-        expect(response.body).toEqual({name: "SecretNotFoundError", message: "Secret Not Found in the System"});
+        expect(response.body).toEqual({
+            name: "SecretNotFoundError",
+            message: "Secret Not Found in the System"
+        });
     });
     it('should retrieve a secret from the system', () => {
     });
